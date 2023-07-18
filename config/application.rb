5d687130997651047c2eb9389ad2a213d012e0b9
Nils van der Werf <n.vanderw.92@gmail.com>
2023-07-18 21:17:02 +1000

    Resolve API requests

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,15 @@ module GigApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+
+    # Set up CORS middleware, needs to go here before the config.api_only line because of Middleware stack order
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+  
     config.api_only = true
   end
 end
